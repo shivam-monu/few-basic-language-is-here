33bd12a49f17f942fa9cdb65fee3b364fcb85379
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-09-03 00:08:34 +0530

    how to print array

---
+++ basics_array.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int marks[90];  //reserve space to store 90 intergers
+    marks[0]=45;
+    marks[1]=50;
+    //we can go all the way till marks[89]
+    printf("marks 0 and marks 1 is %d %d",marks[0],marks[1]);
+}
