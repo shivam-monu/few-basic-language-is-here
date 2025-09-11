d245687fa9f867d44980b05b2096caa15738f1bc
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-09-11 22:17:26 +0530

    using the puts function

---
+++ puts_function.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char s[48];
+    gets(s);
+    printf("%s",s);
+    puts(s);         //print  the string and place the cursor in next line
+    printf("heys");
+
+    return 0;
+}
