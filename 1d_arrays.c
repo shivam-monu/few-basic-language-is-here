7b5077138a0d17b4725ba05fc78c605d05654dad
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-09-16 01:12:00 +0530

    1d array in c program

---
+++ 1d_arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5]={37,57,43,12,45};
+    for (int i = 0; i <5; i++)
+    {
+        
+        printf("print the 1d array i %d is %d\n",i+1,arr[i]);
+    }
+    
+
+    return 0;
+}
